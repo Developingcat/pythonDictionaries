a7b372c222f3a56c344c77a04c44cd18f375ed30
Hannah <148793520+Developingcat@users.noreply.github.com>
2025-03-20 13:39:29 +0000

    worked more on the fruits practice

--- fruitsData.py
+++ fruitsData.py
@@ -1,7 +1,7 @@
 def main():
     # Initialize an empty dictionary to store fruit data
     
-    fruitsDictionary = []
+    fruitsDictionary = {}
 
     # Get the number of fruits to input (input statement). This will be the 
     # number of sets of data you need to collect.
@@ -13,12 +13,32 @@ def main():
     # Collect data for the number of fruits above from the user. Should include name, color, 
     # weight in lbs, and price. Once each set of data points is collected
     # Think about what kind of control structure to create to complete this process
-    fruitsBasket = {'Fruits': "Apple: 70-100 grams, often coming in shades of red, green or sometimes yellow." }
-    
+    for x in range (fruits_number):
+        name = input(f"Enter the fruits name: ").strip()
+        color = input(f"Enter the color of {name}: ").strip()
+        weight = float(input(f"Enter the average weight of {name} (in lbs): ")) 
+        price = float(input(f"Enter in the average price per pound of {name} ($): "))
+
+        # Data storage...wowza.............
+        fruit[name] = {
+            'Color': color, 
+            'Average Weight': weight,
+            'Price Per Pound': price
+        }
+
+    # Output the fruit data 
         # After each set of input statements, store the data in the dictionary
-        
+    print("\n Fruit Data Overview:\n")   
 
     print(fruitsBasket['Fruits'])
+
+    for fruit, details in fruits.item():
+        print(f"Fruit: {fruit}")
+        print(f"Color: {details['Color']}")
+        print(f"Average Weight: {details['Average Weight']}")
+        print(f"Price Per Pound: $ {details['Price Per Pound']}")
+        print("-" * 20)
+
 
     
 
